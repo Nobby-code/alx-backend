5bed2936e2bd580efc0f191e6a5576be3a0d64d9
Nobby-code <norbertguda0@gmail.com>
2023-04-13 10:52:19 -0700

    subscribe and publish

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,23 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on("error", (error)  => {
+ console.log(`Redis client not connected to the server: ${error}`); 
+});
+
+client.on("connect", () => {
+  console.log('Redis client connected to the server');
+});
+
+
+//subscribe
+client.subscribe("holberton school channel");
+
+client.on("message", (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    process.exit(0);
+  }
+});
